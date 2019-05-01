1d0ab3150975b25f41df03fb4c858671bf81b075
Gurkan BURSALI <38786334+GBursali@users.noreply.github.com>
2019-05-01 09:26:51 +0100

    Renaming changes

--- menufier_example.rb
+++ menufier_example.rb
@@ -1,4 +1,4 @@
-require_relative 'gb_menu.rb'
+require_relative 'menufier.rb'
 
 def dog
   puts 'Bark bark'
@@ -8,10 +8,10 @@ def wolf
   puts 'wulf wulf'
 end
 
-main = Menu.new
+menu = Menu.new
 
-main.add(1, 'Dog', method(:dog))
-main.add(2, 'Wolf', method(:wolf))
+menu.add(1, 'Dog', method(:dog))
+menu.add(2, 'Wolf', method(:wolf))
 loop do
-  break unless main.navigated?("Select an animal:")
+  break unless menu.navigated?("Select an animal:")
 end
